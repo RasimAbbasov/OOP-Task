27f97c63bb785313e0ff82ea5694d8c11dfd2cd4
RasimAbbasov <rasimra@code.edu.az>
2024-09-22 16:45:04 +0400

    Delete Task 3 directory

--- Task 3/Sozlerden ibaret arrayde uzunlugu 4-den boyuk olan sozleri ekrana çap edin/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿string[] array = { "Salam", "text", "ddd", "asd", "qwe", "asap", "necesen", "wqaw", "a" };
-for  (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length > 4)
-        Console.WriteLine(array[i]);    
-}
